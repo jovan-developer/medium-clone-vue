35a9fdc2ab4630915d0d5d873b75c7fa409138a8
Jovan <jovan226@mail.ru>
2021-10-15 21:55:38 +0800

    Улучшение страницы регистрации

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,34 +1,40 @@
 import authApi from '@/api/auth'
 
 const state = {
-  isSubmitting: false
+  isSubmitting: false,
+  currentUser: null,
+  validationErrors: null,
+  isLoggedIn: null
 }
 
 const mutations = {
   registerStart(state) {
     state.isSubmitting = true
+    state.validationErrors = null
   },
-  registerSuccess(state) {
+  registerSuccess(state, payload) {
     state.isSubmitting = false
+    state.currentUser = payload
+    state.isLoggedIn = true
   },
-  registerFailure(state) {
+  registerFailure(state, payload) {
     state.isSubmitting = false
+    state.validationErrors = payload
   }
 }
 
 const actions = {
   register(context, credentials) {
-    return new Promise(() => {
+    return new Promise(resolve => {
       context.commit('registerStart')
       authApi
         .register(credentials)
         .then(response => {
-          console.log('response', response)
           context.commit('registerSuccess', response.data.user)
+          resolve(response.data.user)
         })
         .catch(result => {
           context.commit('registerFailure', result.response.data.errors)
-          console.log('Result errors', result)
         })
     })
   }
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,44 +1,44 @@
 <template>
-  <div class="auth-page">
-    <div class="container page">
-      <div class="row">
-        <div class="col-md-6 offset-md-3 col-xs-12">
-          <h1 class="text-xs-center">Sign up</h1>
-          <p class="text-xs-center">
+  <div class='auth-page'>
+    <div class='container page'>
+      <div class='row'>
+        <div class='col-md-6 offset-md-3 col-xs-12'>
+          <h1 class='text-xs-center'>Sign up</h1>
+          <p class='text-xs-center'>
             <router-link :to="{name: 'login'}">Need an account?</router-link>
           </p>
           VALIDATION ERRORS
-          <form @submit.prevent="onSubmit">
-            <fieldset class="form-group">
+          <form @submit.prevent='onSubmit'>
+            <fieldset class='form-group'>
               <input
-                type="text"
-                class="form-control form-control-lg"
-                placeholder="Username"
-                v-model="username"
+                type='text'
+                class='form-control form-control-lg'
+                placeholder='Username'
+                v-model='username'
               />
             </fieldset>
 
-            <fieldset class="form-group">
+            <fieldset class='form-group'>
               <input
-                type="text"
-                class="form-control form-control-lg"
-                placeholder="Email"
-                v-model="email"
+                type='text'
+                class='form-control form-control-lg'
+                placeholder='Email'
+                v-model='email'
               />
             </fieldset>
 
-            <fieldset class="form-group">
+            <fieldset class='form-group'>
               <input
-                type="password"
-                class="form-control form-control-lg"
-                placeholder="Password"
-                v-model="password"
+                type='password'
+                class='form-control form-control-lg'
+                placeholder='Password'
+                v-model='password'
               />
             </fieldset>
 
             <button
-              class="btn btn-lg btn-primary pull-xs-right"
-              :disabled="isSubmitting"
+              class='btn btn-lg btn-primary pull-xs-right'
+              :disabled='isSubmitting'
             >
               Sign up
             </button>
@@ -67,11 +67,16 @@ export default {
   methods: {
     onSubmit() {
       console.log('submitted form')
-      this.$store.dispatch('register', {
-        username: this.username,
-        email: this.email,
-        password: this.password
-      })
+      this.$store
+        .dispatch('register', {
+          username: this.username,
+          email: this.email,
+          password: this.password
+        })
+        .then(user => {
+          console.log('s', user)
+          this.$router.push({name: 'home'})
+        })
     }
   }
 }
